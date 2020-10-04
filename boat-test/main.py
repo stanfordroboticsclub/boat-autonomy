6d990ba3423ae2dad2e6e4cd71fc5d3c450d7f9d
Tejas Narayanan <tejasn100@gmail.com>
2020-10-04 16:30:33 -0700

    Split keyboard input and autonomy into separate functions

--- boat-test/main.py
+++ boat-test/main.py
@@ -12,28 +12,53 @@ from pygame.locals import (
 )
 
 
+def check_for_quit():
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE or event.type == QUIT:
+                env.close()
+        if event.type == QUIT:
+            env.close()
+
+
+def user_control(env):
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE or event.type == QUIT:
+                env.close()
+            if event.key == K_UP:
+                return Action(0, 5)
+            if event.key == K_DOWN:
+                return Action(0, -5)
+            if event.key == K_LEFT:
+                return Action(1, 5)
+            if event.key == K_RIGHT:
+                return Action(1, -5)
+        if event.type == QUIT:
+            env.close()
+
+    return Action(0, 0)
+
+
+def choose_action(env, state):
+    # replace this code with autonomy code; output an Action given the state as input
+    return Action(0, 0)
+
+
 if __name__ == '__main__':
     env = SimpleBoatSim()
     state = env.reset()
 
+    keyboard_control = True
+
     while True:
         action = Action(0, 0)
 
-        # Replace this for loop with autonomy code; output an Action given the state as input.
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE or event.type == QUIT:
-                    env.close()
-                if event.key == K_UP:
-                    action = Action(0, 5)
-                if event.key == K_DOWN:
-                    action = Action(0, -5)
-                if event.key == K_LEFT:
-                    action = Action(1, 5)
-                if event.key == K_RIGHT:
-                    action = Action(1, -5)
-            if event.type == QUIT:
-                env.close()
+        if keyboard_control:
+            action = user_control(env)
+        else:
+            check_for_quit()
+            action = choose_action(env, state)
 
         state = env.step(action)[0]
         env.render()
